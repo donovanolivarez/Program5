60a989099809dec6eb4b0750caca062988935a64
donovanolivarez <34660331+donovanolivarez@users.noreply.github.com>
2019-04-22 19:59:45 -0500

    Final update for PrintFunctions.c
    This should all work well.

--- PrintFunctions.c
+++ PrintFunctions.c
@@ -1,6 +1,12 @@
-/*
- source file for print functions.
-*/
+/*************************************************************** 
+ PrintFunctions.c By Angela Humphries
+ Purpose:
+    This source file holds all of the functions necessary for printing out
+    flight information. prtTraversal and prtTraversalChron are done recursively.
+    The remainder of the print functions are iterative.
+ Notes:
+ 
+****************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
@@ -26,7 +32,40 @@ Notes:
  ****************************************************************/
 void prtTraversal(Graph graph, int iAirportVertex, int indent)
 {
-    // do stuff here.
+    /****  print out successors using depth first ****/
+    EdgeNode *eSuccessor, *ePred;
+    
+    // if successor list is empty, return
+    if (graph->vertexM[iAirportVertex].successorList == NULL)
+        return;
+    
+    // if vertex has been visited, return. Set vertex to visited.
+    if (graph->vertexM[iAirportVertex].bVisited == TRUE)
+        return;
+    
+    graph->vertexM[iAirportVertex].bVisited = TRUE;
+    
+    // loop through successors, print indentations.
+    int i, arrival, nextVertex;
+    for (eSuccessor = graph->vertexM[iAirportVertex].successorList; eSuccessor != NULL;  eSuccessor = eSuccessor->pNextEdge)
+    {
+ 
+        if ((graph->vertexM[eSuccessor->iDestVertex].bVisited == TRUE) && (strcmp(graph->vertexM[eSuccessor->iDestVertex].szAirport, eSuccessor->flight.szDest) == 0))
+            continue;
+        
+        for (i = 0; i < indent; i++)
+        {
+            printf("    ");
+        }
+        
+        // print current info, make recursive call, increase indent by 1.
+        arrival = calcArr2400(eSuccessor->flight.iDepTm2400, eSuccessor->flight.iDurationMins, eSuccessor->flight.iZoneChange);
+        nextVertex = findAirport(graph, eSuccessor->flight.szDest);
+        printf("%s-%s %d-%d\n", eSuccessor->flight.szFlightNr, eSuccessor->flight.szDest, eSuccessor->flight.iDepTm2400, arrival);
+        prtTraversal(graph, nextVertex, indent + 1);
+        
+    }
+
 }
 
 /************************ prtTraversalChron **************************
@@ -50,7 +89,46 @@ Notes:
  ****************************************************************/
 void prtTraversalChron(Graph graph, int iVertex, int iIndent, int iPrevArrTm2400)
 {
-    // do stuff
+    /****  print out successors using depth first ****/
+    EdgeNode *e;
+    
+    // if vertex successor list is NULL, return.
+    if (graph->vertexM[iVertex].successorList == NULL)
+        return;
+    
+    // if vertex has been visited, return. If not, vertex will be set to visited.
+    if (graph->vertexM[iVertex].bVisited == TRUE)
+        return;
+    graph->vertexM[iVertex].bVisited = TRUE;
+    
+    // traverse successors
+    int i, arrival, nextVertex;
+    for (e = graph->vertexM[iVertex].successorList; e != NULL;  e = e->pNextEdge)
+    {
+        
+        // if vertex has been visited and the origin airport is found as a successor, do not print.
+        if ((graph->vertexM[e->iDestVertex].bVisited == TRUE) && (strcmp(graph->vertexM[e->iDestVertex].szAirport, e->flight.szDest) == 0))
+            continue;
+        
+        // if previous arrival time is greater than current flight departure time, not a valid successor. 
+        if (iPrevArrTm2400 + SAFE_DELTA_BETWEEN_FLIGHTS > e->flight.iDepTm2400)
+            continue;
+        
+        //print spaces
+        for (i = 0; i < iIndent; i++)
+        {
+            printf("    ");
+        }
+        
+        // calc arrival time 
+        arrival = calcArr2400(e->flight.iDepTm2400, e->flight.iDurationMins, e->flight.iZoneChange);
+        
+        // calc next vertex. Make recursive call using arrival time. Increase indent by 1.
+        nextVertex = findAirport(graph, e->flight.szDest);
+        printf("%s-%s %d-%d\n", e->flight.szFlightNr, e->flight.szDest, e->flight.iDepTm2400, arrival);
+        prtTraversalChron(graph, nextVertex, iIndent + 1, arrival );
+
+    }
 }
 
 /************************** prtAll *******************************
@@ -66,13 +144,13 @@ void prtAll(Graph graph)
  ****************************************************************/
 void prtAll(Graph graph)
 {
-    // prtOne for every airport
+    // call prtOne for all vertices
     int i;
-    for(i = 0; i < graph->iNumVertices; i++)
+    for (i = 0; i < graph->iNumVertices; i++)
     {
         prtOne(graph, i);
-        printf("\n");
     }
+   
 }
 
 /************************** prtOne *******************************
@@ -94,26 +172,22 @@ void prtOne(Graph graph, int iVertex)
  ****************************************************************/
 void prtOne(Graph graph, int iVertex)
 {
-    int i = 0;
-    int j;
+    // counts of how many successors and predecessors are found.
+    int iSuccessorCnt, iPredCnt;
     
-    // prtPredecessors for each flight
-    i = prtPredecessors(graph, iVertex);
-    j = 8 - i;
+    // total number of dot patterns according to example output.
+    int totalDots = 8;
     
-    // print the dots to separate predecessors from successors
-    prtDots(j);
+    iPredCnt = prtPredecessors(graph, iVertex);
+    totalDots = totalDots - iPredCnt;
+    prtDots(totalDots);
     
-    // reset i
-    i = 0;
-    
-    // prtSuccessors for each flight
-    i = prtSuccessors(graph, iVertex);
-    
-    j = i;
-    // if no successors were printed, print dots
-    if (j = 0)
+    // if no successors, print out 1 pattern of dots.
+    iSuccessorCnt = prtSuccessors(graph, iVertex);
+    if (iSuccessorCnt == 0)
         prtDots(1);
+
+    printf("\n");
 }
 
 /********************** prtFlightsByOrigin ************************
@@ -228,29 +302,24 @@ int  prtPredecessors(Graph graph, int iVertex)
         the correct data.
     2.  A warning will be printed if the airport does not exist.
  ****************************************************************/
-int prtPredecessors(Graph graph, int iVertex)
+int prtPredecessors(Graph g, int iVertex)
 {
-    // local variable declarations
-    char szAirport[MAX_TOKEN]; 
-    char szFlightNr[MAX_TOKEN];
     char szOrigin[MAX_TOKEN];
-    EdgeNode *e;
-    int i;
+    char szFlightNr[MAX_TOKEN];
+    char szAirport[MAX_TOKEN];
+    EdgeNode *p;
+    int i = 0;
     
-    // copy info from airport into local
-    strcpy(szAirport, graph->vertexM[iVertex].szAirport);   
-    printf("%2d %s ", iVertex, szAirport);
+    // copy the airport only once.
+    strcpy(szAirport, g->vertexM[iVertex].szAirport);
     
-    // Traverse through the predecessor list for the specified airport`
-    for (e = graph->vertexM[iVertex].predecessorList; e != NULL; e = e->pNextEdge)
+    printf("%d %s ", iVertex, szAirport);
+    for (p = g->vertexM[iVertex].predecessorList; p != NULL; p = p->pNextEdge)
     {
-        // copy info to local variables for each different flight
-        strcpy(szFlightNr, e->flight.szFlightNr);
-        strcpy(szOrigin,e->flight.szOrigin);
-        
-        // print the airport's predecessors
-        printf("%s/%s ", szFlightNr, szOrigin);
-        
+        // update, should be different each time.
+        strcpy(szOrigin,p->flight.szOrigin);
+        strcpy(szFlightNr, p->flight.szFlightNr);
+        printf("%-s/%s ", szFlightNr, szOrigin);
         i++;
     }
     return i;
@@ -269,37 +338,44 @@ int prtSuccessors(Graph graph, int iVertex)
     1.  This function uses the vertex successor list to print 
         the correct data. It is used in prtOne function.
  ****************************************************************/
-int prtSuccessors(Graph graph, int iVertex)
+int prtSuccessors(Graph g, int iVertex)
 {
-    // local variable declarations
-    char szAirport[MAX_TOKEN]; 
-    char szFlightNr[MAX_TOKEN];
     char szDest[MAX_TOKEN];
-    EdgeNode *e;
+    char szFlightNr[MAX_TOKEN];
+    char szAirport[MAX_TOKEN];
+    EdgeNode *p;
     int i = 0;
     
-    strcpy(szAirport, graph->vertexM[iVertex].szAirport);   
-    printf("%2d %s ", iVertex, szAirport);
+    // copy the airport only once.
+    strcpy(szAirport, g->vertexM[iVertex].szAirport);
     
-    for (e = graph->vertexM[iVertex].successorList; e != NULL; e = e->pNextEdge)
+    //printf("%d %s ", iVertex, szAirport);
+    for (p = g->vertexM[iVertex].successorList; p != NULL; p = p->pNextEdge)
     {
-     
-        strcpy(szFlightNr, e->flight.szFlightNr);
-        strcpy(szDest,e->flight.szDest);
-        
-        // print the airport's predecessors
-        printf("%s-%s\n ", szFlightNr, szDest);
+        // update, should be different each time.
+        strcpy(szDest,p->flight.szDest);
+        strcpy(szFlightNr, p->flight.szFlightNr);
+        printf("%-s-%s ", szFlightNr, szDest);
         i++;
     }
     return i;
 }
 
-/****************/
+/**************************** prtDots ****************************
+ void prtDots(int iCount)
+ Purpose:
+    Prints out a dot pattern
+ Parameters:
+    I int iCount - count of the number of dot patterns to print.
+ Returns:
+    N/A
+ Notes:
+    1. This function is used in prtOne to separate predecessors and successors,
+ *****************************************************************/
 void prtDots(int iCount)
 {
-    while(iCount != 0)
+    for (iCount; iCount > 0; iCount--)
     {
-        printf("..... ");
-        iCount--;
+        printf(".....  ");
     }
 }
